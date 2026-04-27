a726f6822f0355035128a0ee2acd2e87d81c93d2
Jason Livingston <jettoblack@gmail.com>
2026-04-27 10:38:39 -0400

    fix build

--- source/d3d12/d3d12_impl_command_queue.hpp
+++ source/d3d12/d3d12_impl_command_queue.hpp
@@ -39,7 +39,7 @@ namespace reshade::d3d12
 		uint64_t get_timestamp_frequency() const final;
 
 		// 'ID3D12CommandQueue' is thread-safe, so need to lock when accessed from multiple threads
-		std::recursive_mutex _mutex;
+		mutable std::recursive_mutex _mutex;
 
 	private:
 		device_impl *const _device_impl;
